958eddf051058049e3bc3310e793a6a33c17f4a5
kishansaini121 <kishan.saini.900312@gmail.com>
2023-08-05 17:06:01 +0530

    about us page

---
+++ application/views/about_us.php
@@ -0,0 +1,116 @@
+
+    </header>
+    <div class="container">
+        <h1 class="joinbox">Join Our Volunteer Community</h1>
+        <p class="joinboxtext">Welcome to our volunteer page, where the power of altruism and compassion come together
+            to create a positive impact on our community and cause. Volunteering is more than just lending a helping
+            hand; it is a transformative journey that enriches both the giver and the receiver. Become an agent of
+            empowerment, touching lives, and inspiring growth. Join us on this inspiring journey, and let's create a
+            brighter future together!</p>
+        <h1 class="volunteerstar">Our Volunteer Stars</h1>
+        <p class="teambox">Our team of volunteers is a force to be reckoned with! Each one brings their unique skills
+            and unwavering dedication to our cause, creating a powerful collective that drives positive change in our
+            community.</p>
+    </div>
+    <div class="container py-5">
+
+
+        <div class="row text-center">
+            <!-- Team item-->
+            <div class="col-xl-3 col-sm-6 mb-5">
+                <div class="teamcard bg-white rounded shadow-sm py-5 px-4"><img
+                        src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="" width="100"
+                        class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                    <h5 class="mb-0">Manuella Nevoresky</h5><span class="small text-uppercase text-muted">(CEO -
+                        Founder)</span>
+                    <ul class="social mb-0 list-inline mt-3">
+                        <li class="list-inline-item"><a href="#" class="social-link"><i
+                                    class="fa fa-facebook-f"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a>
+                        </li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a>
+                        </li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- End-->
+
+            <!-- Team item-->
+            <div class="col-xl-3 col-sm-6 mb-5">
+                <div class=" teamcard bg-white rounded shadow-sm py-5 px-4"><img
+                        src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="" width="100"
+                        class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                    <h5 class="mb-0">Samuel Hardy</h5><span class="small text-uppercase text-muted">(CEO -
+                        Founder)</span>
+                    <ul class="social mb-0 list-inline mt-3">
+                        <li class="list-inline-item"><a href="#" class="social-link"><i
+                                    class="fa fa-facebook-f"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a>
+                        </li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a>
+                        </li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- End-->
+
+            <!-- Team item-->
+            <div class="col-xl-3 col-sm-6 mb-5">
+                <div class=" teamcard bg-white rounded shadow-sm py-5 px-4"><img
+                        src="https://bootdey.com/img/Content/avatar/avatar8.png" alt="" width="100"
+                        class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                    <h5 class="mb-0">Tom Sunderland</h5><span class="small text-uppercase text-muted">(CEO -
+                        Founder)</span>
+                    <ul class="social mb-0 list-inline mt-3">
+                        <li class="list-inline-item"><a href="#" class="social-link"><i
+                                    class="fa fa-facebook-f"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a>
+                        </li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a>
+                        </li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- End-->
+
+            <!-- Team item-->
+            <div class="col-xl-3 col-sm-6 mb-5">
+                <div class=" teamcard bg-white rounded shadow-sm py-5 px-4"><img
+                        src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="" width="100"
+                        class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                    <h5 class="mb-0">John Tarly</h5><span class="small text-uppercase text-muted">(CEO -
+                        Founder)</span>
+                    <ul class="social mb-0 list-inline mt-3">
+                        <li class="list-inline-item"><a href="#" class="social-link"><i
+                                    class="fa fa-facebook-f"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a>
+                        </li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a>
+                        </li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <h3 class="volunteer">Why Volunteer?</h3>
+        <p>Volunteering is more than just lending a helping hand; it is a transformative journey that enriches both the giver and the receiver. By choosing to volunteer, you become an integral part of a larger collective striving for a better world. It is through this act of selflessness that we experience the true essence of community, where diverse individuals unite to work towards a common goal.
+        </p>
+        <h3 class="volunteer">Benefits of Being a Volunteer:</h3>
+        <p>
+        Beyond the satisfaction of giving back, volunteering offers a multitude of personal benefits. It's an opportunity for personal growth and development, helping you build valuable skills, boost self-confidence, and expand your network. Volunteering can also serve as a welcome break from daily routines, providing a sense of purpose and fulfillment that reinvigorates your spirit.
+        </p>
+        <h3 class="volunteer">How Can You Join?</h3>
+        <p>Becoming a volunteer is simple and rewarding. We offer a diverse range of volunteer roles and opportunities, catering to various interests and availability. Whether you can spare a few hours a week or dedicate more extensive time, your commitment is valued and appreciated. To get started, click the "Join Now" button below, and embark on a meaningful journey that makes a difference in the lives of many.
+        </p>
+        <p class="thanks">Thank you for considering joining our volunteer community. Together, we can build a world fueled by compassion, empathy, and positive change. Join us on this inspiring journey, and let's create a brighter future together!</p>
+
+    </div>
